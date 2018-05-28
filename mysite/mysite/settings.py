43b109245e3d30b1ac21c29deb4c40d3efc275ba
teejaytanmay <tanmay.jain@iiitb.org>
2018-05-28 13:54:42 +0530

    allowed_hosts

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c9+c+f6rt=!i3l9bs#p-4l!&ax+yhjh_+(kz7b9pefu%nydy_8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
